0e2237fe856955b98048adbc0e65491ff4e53dcb
GalaxyShad <vadim.kadyrin@mail.ru>
2022-10-17 21:36:30 +0500

    feat: started first screen

--- ScheduleApp/values/styles.js
+++ ScheduleApp/values/styles.js
@@ -1,9 +1,15 @@
+import { StyleSheet } from 'react-native';
+
+
 export const styles = StyleSheet.create({
     container: {
         flex: 1,
+        flexDirection: 'column',
         backgroundColor: '#202020',
         alignItems: 'center',
         justifyContent: 'center',
+
+
     },
     text: {
         color: '#707C82',
@@ -12,4 +18,4 @@ export const styles = StyleSheet.create({
     cell: {
         backgroundColor: "#202020"
     },
-});
+})
